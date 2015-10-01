d37514af88c9acfc758b103d0e23a5592900b8a8
Karthik Vasudevan <mithralaya@gmail.com>
2015-10-01 19:33:26 +0100

    Added a class called KVBubbleChart, added constructor with holds and inits options

---
+++ KVCharts/KVBubbleChart.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Created By Karthik Vasudevan
+ * On 01/10/2015
+ * A simple class to hold svg elements
+ */
+
+// closure to avoid external variable conflicts
+var KVBubbleChart = (function(window, document){
+
+    /**
+     * @name KVBubbleChart
+     * @description
+     * Constructor to load public & private variables and default functions
+     *
+     * @param options {Object}
+     * @returns void.
+     */
+    var KVBubbleChart = function(options)
+    {
+
+    };
+
+    /**
+     * @type KVBubbleChart prototypes public functions
+     */
+    KVBubbleChart.prototype =
+    {
+
+    }
+
+})(window, document);
