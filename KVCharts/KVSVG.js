d0aab2e22e1d9ee2c8940fd52896de606bd05324
Karthik Vasudevan <mithralaya@gmail.com>
2015-10-04 20:08:32 +0100

    created a class which will hold all SVG elements with properties

---
+++ KVCharts/KVSVG.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Created By Karthik Vasudevan
+ * On 01/10/2015
+ * A simple class to hold svg elements
+ */
+
+// closure to avoid external variable conflicts
+var KVSVG = (function(window, document){
+
+    /**
+     * @name KVSVG
+     * @description
+     * Constructor to load public & private variables and default functions
+     *
+     * @param none
+     * @returns void.
+     */
+    var KVSVG = function()
+    {
+
+    };
+
+    /**
+     * @description
+     * KVSVG prototypes public functions
+     */
+    KVSVG.prototype = {
+
+    };
+
+    return KVSVG;
+
+})(window, document);
