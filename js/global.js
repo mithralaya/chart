fc3e5708d4e45e4f539f8df25a233d45cb1cc203
Karthik Vasudevan <mithralaya@gmail.com>
2015-10-05 03:00:43 +0100

    add a company feature done

--- js/global.js
+++ js/global.js
@@ -36,10 +36,29 @@ var options = {
     },
     data: data
 };
+var sectors = ["Energy", "Automobile", "Services", "Agriculture", "Commodities"];
+
 function init()
 {
     new KVBubbleChart(options, document.getElementById("bubbleChart"));
 }
+
+function addCompany()
+{
+    var newCompany = {
+        "incopDate": getRandomInt(465599466000, 1443820266000),
+        "rev": Math.ceil((getRandomInt(40, 1100)+1)/10)*10,
+        "val": Math.ceil((getRandomInt(1000, 30000)+1)/100)*100,
+        "sect": sectors[Math.floor(Math.random()*sectors.length)]
+    };
+    data.push(newCompany);
+    init();
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 
 window.onresize = function(){init()};
